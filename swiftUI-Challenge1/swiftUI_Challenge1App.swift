2824699ecab15910a52d7fe6c76d792a3fde6589
kabikira <kabikirar2@protonmail.com>
2023-01-04 11:06:35 +0900

    Initial Commit

---
+++ swiftUI-Challenge1/swiftUI_Challenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_Challenge1App.swift
+//  swiftUI-Challenge1
+//
+//  Created by koala panda on 2023/01/04.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_Challenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
